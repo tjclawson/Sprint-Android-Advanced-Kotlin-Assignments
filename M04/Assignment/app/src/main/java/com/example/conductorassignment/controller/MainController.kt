55f06ae31fb75e75c9a16e9f1493d25e5379e955
tjclawson <tjamesclawson@gmail.com>
2019-10-17 18:24:37 -0400

    Assignment Complete

--- M04/Assignment/app/src/main/java/com/example/conductorassignment/controller/MainController.kt
+++ M04/Assignment/app/src/main/java/com/example/conductorassignment/controller/MainController.kt
@@ -10,9 +10,23 @@ import com.bluelinelabs.conductor.ControllerChangeHandler
 import com.bluelinelabs.conductor.ControllerChangeType
 import com.bluelinelabs.conductor.RouterTransaction
 import com.bluelinelabs.conductor.changehandler.HorizontalChangeHandler
+import com.bluelinelabs.conductor.changehandler.VerticalChangeHandler
 import com.example.conductorassignment.R
 
-class MainController : Controller() {
+class MainController : Controller(), NumberController.NumberContainer, TextController.TextContainer {
+
+    var receivedData: Int? = null
+    var recievedText: String? = null
+
+    override fun getData(data: Int) {
+        receivedData = data
+        recievedText = null
+    }
+
+    override fun getText(text: String) {
+        recievedText = text
+        receivedData = null
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
         val view = inflater.inflate(R.layout.main_controller_layout, container, false)
@@ -25,17 +39,22 @@ class MainController : Controller() {
 
         val buttonText = view?.findViewById<Button>(R.id.button_launch_text_controller)
         val buttonNumber = view?.findViewById<Button>(R.id.button_launch_number_controller)
+        val textView = view?.findViewById<TextView>(R.id.text_view_main_controller)
+        textView?.text = receivedData?.toString() ?: recievedText
+        textView?.text = recievedText?.toString() ?: receivedData.toString()
+
+
 
         buttonText?.setOnClickListener {
-            router.pushController(RouterTransaction.with(TextController())
+            router.pushController(RouterTransaction.with(TextController(this))
                 .pushChangeHandler(HorizontalChangeHandler())
                 .popChangeHandler(HorizontalChangeHandler()))
         }
 
         buttonNumber?.setOnClickListener {
-            router.pushController(RouterTransaction.with(NumberController())
-                .pushChangeHandler(HorizontalChangeHandler())
-                .popChangeHandler(HorizontalChangeHandler()))
+            router.pushController(RouterTransaction.with(NumberController(this))
+                .pushChangeHandler(VerticalChangeHandler())
+                .popChangeHandler(VerticalChangeHandler()))
         }
     }
 }
--- M04/Assignment/app/src/main/java/com/example/conductorassignment/controller/TextController.kt
+++ M04/Assignment/app/src/main/java/com/example/conductorassignment/controller/TextController.kt
@@ -3,14 +3,42 @@ package com.example.conductorassignment.controller
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import com.bluelinelabs.conductor.Controller
+import com.bluelinelabs.conductor.ControllerChangeHandler
+import com.bluelinelabs.conductor.ControllerChangeType
 import com.example.conductorassignment.R
 
-class TextController : Controller() {
+class TextController<T>(): Controller()
+    where T: Controller, T: TextController.TextContainer{
+
+    constructor(targetController: T): this() {
+        setTargetController(targetController)
+    }
+
+    interface TextContainer {
+        fun getText(text: String)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
         val view = inflater.inflate(R.layout.text_controller_layout, container, false)
         return view
+    }
+
+    override fun onChangeEnded(changeHandler: ControllerChangeHandler, changeType: ControllerChangeType) {
+        super.onChangeEnded(changeHandler, changeType)
+
+        val buttonText = view?.findViewById<Button>(R.id.button_send_text)
+        val etText = view?.findViewById<EditText>(R.id.et_text_controller)
+
+        buttonText?.setOnClickListener {
+            val text = etText?.text.toString()
+
+            (targetController as TextContainer).getText(text)
+            router.popCurrentController()
+
+        }
     }
 
 }
